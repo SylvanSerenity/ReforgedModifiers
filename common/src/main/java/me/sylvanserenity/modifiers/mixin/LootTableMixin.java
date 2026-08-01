03a041db66a313c3d566113a83a0cb9546f9485d
sylvan <sylvanserene@gmail.com>
2026-07-31 17:49:11 -0700

    Random modifier on loot.

---
+++ common/src/main/java/me/sylvanserenity/modifiers/mixin/LootTableMixin.java
@@ -0,0 +1,24 @@
+package me.sylvanserenity.modifiers.mixin;
+
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import me.sylvanserenity.modifiers.modifier.ModifierHandler;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootTable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LootTable.class)
+public class LootTableMixin {
+    @Inject(
+        method = "getRandomItems(Lnet/minecraft/world/level/storage/loot/LootContext;)Lit/unimi/dsi/fastutil/objects/ObjectArrayList;",
+        at = @At("RETURN")
+    )
+    private void modifiers$getRandomItems(LootContext context, CallbackInfoReturnable<ObjectArrayList<ItemStack>> cir) {
+        for (ItemStack stack : cir.getReturnValue()) {
+            ModifierHandler.applyRandom(stack, context.getRandom());
+        }
+    }
+}
